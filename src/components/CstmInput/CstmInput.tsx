3b28eb4e70cb3a0459f227ef7a804f5551ea4e13
dhelfy <dhcode4business@gmail.com>
2024-11-07 17:57:16 +0500

    small changes in CstmInput component structure

--- src/components/CstmInput/CstmInput.tsx
+++ src/components/CstmInput/CstmInput.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../state/store";
 import { setInputValue } from "../../state/slices/bookSlice";
+import { inputValueSelector } from "../../state/selectors/bookSelector";
 
 interface CstmInputProps {
     placeholder?: string;
@@ -11,14 +12,18 @@ interface CstmInputProps {
 
 export const CstmInput: FC<CstmInputProps> = ({placeholder, name}) => {
     // useSelector вынести в селекторы
-    const inputValue = useSelector((state: RootState) => state.bookReducer.inputs[name])
+    const inputValue = useSelector(inputValueSelector)
     const dispatch: AppDispatch = useDispatch()
 
-    // вынести onChange в функцию
+    // вынес onChange в функцию
+    function onChangeFunction(e: React.ChangeEvent<HTMLInputElement>) {
+        dispatch(setInputValue({name: name, value: e.currentTarget.value}))
+    }
+
     return (
         <input 
-         value={inputValue}
-         onChange={(e) => {dispatch(setInputValue({name: name, value: e.currentTarget.value}))}}
+         value={inputValue[name]}
+         onChange={onChangeFunction}
          placeholder={placeholder}
         />
     )
--- src/state/selectors/bookSelector.ts
+++ src/state/selectors/bookSelector.ts
@@ -2,5 +2,9 @@ import { createSelector } from "@reduxjs/toolkit"
 import { RootState } from "../store"
 
 const root = (state: RootState) => state.bookReducer
+// сделал так, потому что если сделать через createSelector, то в консоли Redux ругается
+// и говорит что финальное значение createSelector не модифицируется, что может приводить
+// к ненужной мемоизации и лишним ререндерам
+export const inputValueSelector = (state: RootState) => state.bookReducer.inputs
 
 export const selectAllBooks = createSelector(root, (data) => {return data.books})
